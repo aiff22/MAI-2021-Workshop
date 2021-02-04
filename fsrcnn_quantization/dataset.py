77cb0b94be3b96a067f38c20d9b161d6271d9556
Andrew Lek <andrew.lek@synaptics.com>
2021-02-03 16:05:38 -0800

    Support training and quantization using the DIV2K dataset
    - DIV2K dataset can be obtained from https://data.vision.ee.ethz.ch/cvl/DIV2K/
    - Add DIV2K data generator
    - Change upscaling factor of the demo to x3
    - Save hires image of evaluation image to disk

--- fsrcnn_quantization/dataset.py
+++ fsrcnn_quantization/dataset.py
@@ -3,28 +3,74 @@ import numpy as np
 import tensorflow as tf
 import cv2
 
-class TrainDataset(tf.keras.utils.Sequence):
-    def __init__(self, h5_file, batch_size=32):
-        self.h5_file = h5_file
+class DIV2K(tf.keras.utils.Sequence):
+    def __init__(self, data_root, scale_factor=3, batch_size=32, patch_size=48, type='train'):
+        self.data_root = data_root
+        self.scale_factor = scale_factor
         self.batch_size = batch_size
+        self.image_ids = range(1, 801)
+        self.patch_size = patch_size
+        self.type = 'train'
 
     def __getitem__(self, idx):
-        with h5py.File(self.h5_file, 'r') as f:
-            return np.expand_dims(f['lr'][idx * self.batch_size:(idx + 1) * self.batch_size] / 255., -1), np.expand_dims(f['hr'][idx * self.batch_size:(idx + 1) * self.batch_size] / 255., -1)
+        if self.patch_size > 0:
+            start = idx * self.batch_size
+            end = (idx + 1) * self.batch_size
+            batch_ids = self.image_ids[start:end]
+            lr_batch = np.zeros((self.batch_size, self.patch_size // self.scale_factor, self.patch_size // self.scale_factor, 1), dtype='float32')
+            hr_batch = np.zeros((self.batch_size, self.patch_size, self.patch_size, 1), dtype='float32')
+            for i, id in enumerate(batch_ids):
+                lr, hr = self._get_image_pair(id)
+                lr = np.expand_dims(lr, 0)
+                lr = np.expand_dims(lr, -1)
+                hr = np.expand_dims(hr, 0)
+                hr = np.expand_dims(hr, -1)
+                lr_batch[i] = lr
+                hr_batch[i] = hr
+            return lr_batch, hr_batch
+        else:
+            # Return 1 image pair if not returning an image patch
+            return self._get_image_pair(self.image_ids[idx])
 
     def __len__(self):
-        with h5py.File(self.h5_file, 'r') as f:
-            return len(f['lr']) // self.batch_size
+        if self.patch_size > 0:
+            length = int(np.ceil(len(self.image_ids) / self.batch_size))
+        else:
+            length = 1
+        return length
+
+    def _get_image_pair(self, id):
+        image_id = f'{id:04}'
+        hr_file = f'{self.data_root}/DIV2K_{self.type}_HR/{image_id}.png'
+        hr_y = self._get_y_chan(hr_file).astype(np.float32)
+        if self.patch_size > 0:
+            hr_y = self._crop_center(hr_y, self.patch_size, self.patch_size)
+        hr_y = hr_y / 255.
+        lr_file = f'{self.data_root}/DIV2K_{self.type}_LR_bicubic/X{self.scale_factor}/{image_id}x{self.scale_factor}.png'
+        lr_y = self._get_y_chan(lr_file).astype(np.float32)
+        if self.patch_size > 0:
+            lr_y = self._crop_center(lr_y, self.patch_size // self.scale_factor, self.patch_size // self.scale_factor)
+        lr_y = lr_y / 255.
+        return lr_y, hr_y
+
+    def _get_y_chan(self, image_path):
+        im = cv2.imread(image_path, -1)
+        yuv = cv2.cvtColor(im, cv2.COLOR_BGR2YUV)
+        y, _, _ = cv2.split(yuv)
+        return y
+
+    def _crop_center(self, im, crop_h, crop_w):
+        startx = im.shape[1] // 2 - (crop_w // 2)
+        starty = im.shape[0] // 2 - (crop_h // 2)
+        return im[starty:starty + crop_h, startx:startx + crop_w]
 
 def representative_dataset_gen():
-    with h5py.File('datasets/div2k_x4_sample.h5', 'r') as f:
-        for lr in f['lr']:
-            yuv = cv2.cvtColor(lr, cv2.COLOR_BGR2YUV)
-            y, _, _ = cv2.split(yuv)
-            y = y.astype(np.float32) / 255.
-            y = np.expand_dims(y, 0)
-            y = np.expand_dims(y, -1)
-            yield [y]
+    div2k = DIV2K('datasets/DIV2K')
+    batch_x, _ = div2k[0]
+    for x in batch_x:
+        x = np.expand_dims(x, 0)
+        print(x.shape)
+        yield [x]
 
 def representative_dataset_gen_image_shapes():
     with h5py.File('datasets/div2k_x4_sample.h5', 'r') as f:
--- fsrcnn_quantization/demo.py
+++ fsrcnn_quantization/demo.py
@@ -1,11 +1,13 @@
 import os
 import argparse
 import cv2
+import numpy as np
 import fsrcnn
 
-SCALE_FACTOR = 4
+SCALE_FACTOR = 3
 SAVED_MODEL_PATH = 'fsrcnn_saved_model'
-TFLITE_MODEL_PATH = 'fsrcnn_x4_ptq.tflite'
+TFLITE_MODEL_PATH = f'fsrcnn_x{SCALE_FACTOR}_ptq.tflite'
+TFLITE_MODEL_INPUT_SHAPE = [1, 360, 640, 1]
 
 if __name__ == '__main__':
 
@@ -13,21 +15,21 @@ if __name__ == '__main__':
     parser.add_argument('--train', dest='train', action='store_true')
     parser.add_argument('--quantize', dest='quantize', action='store_true')
     parser.add_argument('--eval', dest='eval', action='store_true')
-    parser.add_argument('--train-data', type=str, default='datasets/91-image_x4.h5')
+    parser.add_argument('--div2k-root', type=str, default='datasets/DIV2K')
     parser.add_argument('--saved-model-path', type=str, default='fsrcnn_saved_model')
-    parser.add_argument('--quant-model-path', type=str, default='fsrcnn_x4_ptq.tflite')
+    parser.add_argument('--quant-model-path', type=str, default='fsrcnn_x3_ptq.tflite')
     parser.add_argument('--eval-index', type=int, default=0)
     args = parser.parse_args()
 
     if args.train:
         print('TRAIN')
-        if not os.path.exists(args.train_data):
-            print(f'Train data not found: {args.train_data}')
+        if not os.path.exists(args.div2k_root):
+            print(f'Train data not found: {args.div2k_root}')
             raise ValueError
         print(f'Preparing FSRCNN super resolution model with scale factor {SCALE_FACTOR}')
         model = fsrcnn.build(scale_factor=SCALE_FACTOR)
-        print(f'Train with dataset {args.train_data}')
-        model = fsrcnn.train(model, args.train_data, num_epochs=1, batch_size=64)
+        print(f'Train with dataset {args.div2k_root}')
+        model = fsrcnn.train(model, args.div2k_root, scale_factor=SCALE_FACTOR, num_epochs=5, batch_size=32)
         print(f'Saving trained SavedModel to {SAVED_MODEL_PATH}')
         model.save(SAVED_MODEL_PATH, overwrite=True, include_optimizer=False, save_format='tf')
         
@@ -39,7 +41,7 @@ if __name__ == '__main__':
             print(f'SavedModel not found at {args.saved_model_path}')
             raise ValueError
         print('Start quantization')
-        quant_model = fsrcnn.quantize(args.saved_model_path)
+        quant_model = fsrcnn.quantize(args.saved_model_path, scale_factor=SCALE_FACTOR, data_path=args.div2k_root, input_shape=TFLITE_MODEL_INPUT_SHAPE)
         print(f'Saving quantized model to {TFLITE_MODEL_PATH}')
         open(TFLITE_MODEL_PATH, 'wb').write(quant_model)
 
@@ -49,8 +51,9 @@ if __name__ == '__main__':
             print(f'Quantized model file not found at {args.quant_model_path}')
             raise ValueError
         print(f'Evaluating image {args.eval_index} on {args.quant_model_path}')
-        sr_y, psnr = fsrcnn.evaluate(args.quant_model_path, image_index=args.eval_index)
+        sr_y, hr_y, psnr = fsrcnn.evaluate(args.quant_model_path, args.div2k_root, image_index=args.eval_index)
         print(f'Eval index {args.eval_index} psnr: {psnr:.2f}')
         output_file = f'sr_y_{args.eval_index}.png'
         print(f'Saving upscaled Y-channel to {output_file}') 
         cv2.imwrite(output_file, cv2.merge([sr_y, sr_y, sr_y]))
+        cv2.imwrite(f'hr_y_{args.eval_index}.png', cv2.merge([hr_y, hr_y, hr_y]))
